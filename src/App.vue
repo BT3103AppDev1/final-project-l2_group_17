61c66fe41a934088c11e3c3642f00b2e4e23cc49
Min Xuan <e1354590@u.nus.edu>
2026-03-23 17:53:11 +0800

    Added components, router, views, firebase

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<script setup>
+import { computed, onMounted, onUnmounted, ref } from 'vue'
+import { onAuthStateChanged } from 'firebase/auth'
+import { auth } from './firebase'
+import Login from '@/views/Login.vue'
+</script>
+
+<template>
+  <Login />
+</template>
+
+<style scoped></style>
---
+++ src/views/Login.vue
@@ -0,0 +1,8 @@
+<template>
+  <div>
+    <p>Login Placeholder</p>
+  </div>
+</template>
+
+<script setup>
+</script>
