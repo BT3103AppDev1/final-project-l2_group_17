45d762fc715e0b3a0531145e05ac7f454b9d5958
Blueshocker124 <jainelinje@gmail.com>
2026-03-28 22:02:06 +0800

    merge origin/main into feature/menu

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,11 @@
 <script setup>
 import "./assets/global.css"
-import Login from '@/views/LoginPage.vue'
-import CustomerMenu from './views/CustomerMenu.vue'
 import { RouterView } from 'vue-router'
 
 </script>
 
 <template>
   <RouterView />
-  <Login />
-  <CustomerMenu />
 </template>
 
 <style scoped></style>
