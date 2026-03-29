94a739d493538a0b8cb887786528401cbdd9be5c
Min Xuan <e1354590@u.nus.edu>
2026-03-30 00:06:08 +0800

    Standardise naming as admin instead of owner

--- src/views/AdminProfile.vue
+++ src/views/AdminProfile.vue
@@ -1,11 +1,11 @@
 <script setup>
-import OwnerProfile from '@/components/OwnerProfile.vue'
+import AdminProfile from '@/components/AdminProfile.vue'
 import NavOwner from '@/components/NavOwner.vue';
 </script>
 
 <template>
   <div>
     <NavOwner />
-    <OwnerProfile />
+    <AdminProfile />
   </div>
 </template>
